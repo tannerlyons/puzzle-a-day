41d40fe902de65da40effb25dd480b950ead37b9
Tanner Lyons <tanner.lyons@smartsheet.com>
2022-11-14 22:28:17 -0800

    remove all web stuff

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,7 @@
-const { resolve } = require("path");
-
-// Can add an env var in gitlab-ci to override this.
-// Starting nothing to hopefully have smaller barriers.
-const coveragethreshold = global.JEST_COVERAGE_THRESHOLD || 0;
-
 module.exports = {
     testEnvironment: "jsdom",
-    // setupFilesAfterEnv: [resolve(__dirname, "config/jest.setup.ts")],
-    // https://jestjs.io/docs/en/configuration#coveragethreshold-object
-    coverageThreshold: {
-        global: {
-            branches: coveragethreshold,
-            functions: coveragethreshold,
-            lines: coveragethreshold,
-            statements: coveragethreshold,
-        },
-    },
     transform: {
-        "^.+\\.tsx?$": "babel-jest",
+        "^.+\\.tsx?$": "ts-jest",
     },
     testMatch: ["**/*.test.(ts|tsx)"],
     moduleDirectories: ["src", "node_modules"],
